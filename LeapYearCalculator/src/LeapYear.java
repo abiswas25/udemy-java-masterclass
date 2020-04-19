701ad107a1d2be6e14480924e4ebb496636c1215
abiswas25 <anuvab.biswas@ymail.com>
2020-04-19 17:11:38 +0530

    alternate logic for Leap Year Calculator coding exercise

--- LeapYearCalculator/src/LeapYear.java
+++ LeapYearCalculator/src/LeapYear.java
@@ -2,11 +2,20 @@
 public class LeapYear {
 	
 	public static boolean isLeapYear(int year) {
+//		if((year < 1 || year > 9999) || ((year % 4) != 0)) {
+//			return false;
+//		} else if((year % 100) != 0) {
+//			return true;
+//		} else if((year % 400) == 0) {
+//			return true;
+//		} else {
+//			return false;
+//		}
 		if((year < 1 || year > 9999) || ((year % 4) != 0)) {
 			return false;
-		} else if((year % 100) != 0) {
-			return true;
-		} else if((year % 400) == 0) {
+		}
+		year = ((year % 100) == 0) ? (year / 100) : year;
+		if((year % 4) == 0) {
 			return true;
 		} else {
 			return false;
