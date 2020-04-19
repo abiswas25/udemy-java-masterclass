cbd9598b050bc1f69200ffd0c456dae14202df6e
abiswas25 <anuvab.biswas@ymail.com>
2020-04-19 13:29:11 +0530

    solution to SpeedConverter coding exercise

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author abviper25
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		SpeedConverter.printConversion(1.5);
+		SpeedConverter.printConversion(10.25);
+		SpeedConverter.printConversion(-5.6);
+		SpeedConverter.printConversion(25.45);
+		SpeedConverter.printConversion(75.114);
+
+	}
+
+}
