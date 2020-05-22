d3c381eff1bd80aa8413a316f6fe55a914d27919
abiswas25 <anuvab.biswas@ymail.com>
2020-05-22 13:52:03 +0530

    solutions for Even Digit Sum and SHared Digit coding exercises

--- CodingExercises/src/Main.java
+++ CodingExercises/src/Main.java
@@ -8,11 +8,20 @@ public class Main {
 //		System.out.println(NumberPalindrome.isPalindrome(11212));
 //		System.out.println(NumberPalindrome.isPalindrome(1));
 		
-		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));
-		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(257));
-		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));
-		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
-		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-252));
+//		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));
+//		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(257));
+//		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));
+//		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
+//		System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-252));
+		
+//		System.out.println(EvenDigitSum.getEvenDigitSum(123456789));
+//		System.out.println(EvenDigitSum.getEvenDigitSum(252));
+//		System.out.println(EvenDigitSum.getEvenDigitSum(-22));
+		
+		System.out.println(SharedDigit.hasSharedDigit(12,  23));
+		System.out.println(SharedDigit.hasSharedDigit(9, 99));
+		System.out.println(SharedDigit.hasSharedDigit(15, 55));
+		System.out.println(SharedDigit.hasSharedDigit(13, 45));
 
 	}
 
