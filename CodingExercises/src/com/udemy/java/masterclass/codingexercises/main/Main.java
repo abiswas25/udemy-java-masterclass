0e7d514e00c34b8ed261ac1a8e8fb9aafcb52744
abiswas25 <anuvab.biswas@ymail.com>
2020-06-04 19:56:43 +0530

    solution to coding exercise 35, 36

--- CodingExercises/src/com/udemy/java/masterclass/codingexercises/main/Main.java
+++ CodingExercises/src/com/udemy/java/masterclass/codingexercises/main/Main.java
@@ -1,6 +1,7 @@
 package com.udemy.java.masterclass.codingexercises.main;
 
-import com.udemy.java.masterclass.codingexercises.section06.ComplexNumber;
+import com.udemy.java.masterclass.codingexercises.section06.Cuboid;
+import com.udemy.java.masterclass.codingexercises.section06.Rectangle;
 
 public class Main {
 
@@ -143,17 +144,37 @@ public class Main {
 //		calculator = new Calculator(floor, carpet);
 //		System.out.println("total= " + calculator.getTotalCost());
 
-		ComplexNumber one = new ComplexNumber(1.0, 1.0);
-		ComplexNumber number = new ComplexNumber(2.5, -1.5);
-		one.add(1, 1);
-		System.out.println("one.real= " + one.getReal());
-		System.out.println("one.imaginary= " + one.getImaginary());
-		one.subtract(number);
-		System.out.println("one.real= " + one.getReal());
-		System.out.println("one.imaginary= " + one.getImaginary());
-		number.subtract(one);
-		System.out.println("number.real= " + number.getReal());
-		System.out.println("number.imaginary= " + number.getImaginary());
+//		ComplexNumber one = new ComplexNumber(1.0, 1.0);
+//		ComplexNumber number = new ComplexNumber(2.5, -1.5);
+//		one.add(1, 1);
+//		System.out.println("one.real= " + one.getReal());
+//		System.out.println("one.imaginary= " + one.getImaginary());
+//		one.subtract(number);
+//		System.out.println("one.real= " + one.getReal());
+//		System.out.println("one.imaginary= " + one.getImaginary());
+//		number.subtract(one);
+//		System.out.println("number.real= " + number.getReal());
+//		System.out.println("number.imaginary= " + number.getImaginary());
+
+//		Circle circle = new Circle(3.75);
+//		System.out.println("circle.radius= " + circle.getRadius());
+//		System.out.println("circle.area= " + circle.getArea());
+//		Cylinder cylinder = new Cylinder(5.55, 7.25);
+//		System.out.println("cylinder.radius= " + cylinder.getRadius());
+//		System.out.println("cylinder.height= " + cylinder.getHeight());
+//		System.out.println("cylinder.area= " + cylinder.getArea());
+//		System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+		Rectangle rectangle = new Rectangle(5, 10);
+		System.out.println("rectangle.width= " + rectangle.getWidth());
+		System.out.println("rectangle.length= " + rectangle.getLength());
+		System.out.println("rectangle.area= " + rectangle.getArea());
+		Cuboid cuboid = new Cuboid(5, 10, 5);
+		System.out.println("cuboid.width= " + cuboid.getWidth());
+		System.out.println("cuboid.length= " + cuboid.getLength());
+		System.out.println("cuboid.area= " + cuboid.getArea());
+		System.out.println("cuboid.height= " + cuboid.getHeight());
+		System.out.println("cuboid.volume= " + cuboid.getVolume());
 
 	}
 
